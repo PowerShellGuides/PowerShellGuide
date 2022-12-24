fae9a27f6015f7c804abc0fa3bf4de4f8c668873
James Brundage <@github.com>
2022-12-24 02:39:12 -0800

    Enabling Topic Metadata (Fixes #10)

---
+++ Types/PowerShell.Guide.Topic.File/get_Metadata.ps1
@@ -0,0 +1,21 @@
+if ($this.psobject.properties['_CachedMetadata']) {
+    return $this._CachedMetadata
+}
+
+$psd1FileName = $this.Name     + '.psd1'
+$psd1File     = $this.Fullname + '.psd1'
+$topicData    = 
+    if (Test-Path $psd1File) {
+        try {
+            Import-LocalizedData -BaseDirectory $this.Directory.Fullname -FileName $psd1FileName
+        } catch {
+            Write-Warning "$_"
+            @{}
+        }
+    } else {
+        @{}
+    }
+
+$topicData["title"] = $this.TopicName
+$this | Add-Member NoteProperty _CachedMetadata $topicData -Force
+return $topicData
